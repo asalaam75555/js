5fbd201a409827821fa3879730807778eda2f7c3
Abdul Salaam <asalaam75555@gmail.com>
2024-08-26 12:41:27 +0530

    add conversion operation file

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "abdul"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNummber = Number(score)
+//console.log(typeof valueInNummber);
+//console.log(valueInNummber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0 
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "hitesh" => true ; "" => false
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
