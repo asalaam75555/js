7fe240640a516c0f74a7559833f332d1215bfaf0
Abdul Salaam <asalaam75555@gmail.com>
2024-09-15 00:36:59 +0530

    added comparison file

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,24 @@
+// console.log(1 > 2);
+// console.log(1 < 2);
+// console.log(1 >= 2);
+// console.log(1 <= 2);
+// console.log(1 == 2);
+// console.log(1 != 2);
+
+
+// console.log(null > 0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true
+
+//---------undefined has simalar problems while comparing operator becaues of its type conversion------------
+
+// console.log(undefined == 0); //false
+// console.log(undefined > 0); //false
+// console.log(undefined < 0); //false
+
+/*----------------------------because of the conversion probelm in equality check JS has === strick 
+equality check to check check the Data type and aswell as the value---------------------------*/
+
+console.log("2" === 2); // 
+
+
