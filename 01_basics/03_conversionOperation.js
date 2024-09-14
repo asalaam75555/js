5dd847cee067b7391f6e537936aea507a8edde5e
Abdul Salaam <asalaam75555@gmail.com>
2024-09-14 23:58:16 +0530

    modified conversion operation file

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,3 +26,40 @@ console.log(stringNumber);
 console.log(typeof stringNumber);
 
 
+//---------------------------**********Operations*********-----------------------------------
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "Abdul"
+let  str2 = "Salaam"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+// console.log((1 + 3) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
